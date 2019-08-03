820c8f340d308a5507bca139142fe1920a0de95a
Camille <camille.durif@edu.gobelins.fr>
2019-08-03 13:31:14 +0200

    Player controller

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,27 +11,31 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
-        health.Initialize();
+        //health.Initialize();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+
+        transform.Translate(movement*speed);
     }
 
     public void TakeDamage(int damage)
     {
-        health.CurrentVal -= damage;
+        /*health.CurrentVal -= damage;
         if (health.CurrentVal <= 0)
         {
             Destroy(gameObject);
-        }
+        }*/
     }
 }
