b34d68053f5ff0cf222bfb20e3a61e97890792e3
Camille <camille.durif@edu.gobelins.fr>
2019-08-03 09:25:53 +0200

    Set up

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 force = new Vector2(horizontal, vertical);
+
+        this.transform.Translate(force*speed);
+    }
+}
