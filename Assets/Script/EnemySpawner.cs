0978047bde3385d6d7dd2f432e33e8a6a495da0a
Camille <camille.durif@edu.gobelins.fr>
2019-08-03 14:02:19 +0200

    Enemies (navmesh + AI)

---
+++ Assets/Script/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public int nbOfEnememies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawEnemies());
+    }
+
+    private IEnumerator SpawEnemies()
+    {
+        for (int i = 0; i < nbOfEnememies; i++)
+        {
+            Instantiate(enemy, this.transform.position, this.transform.rotation);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
