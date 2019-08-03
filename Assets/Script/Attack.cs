b9cb9decead6e33c8de57ef76f1484cb8e0d92a5
Romain Dannin <romain.dannin@gmail.com>
2019-08-03 09:55:45 +0200

    one more script

---
+++ Assets/Script/Attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour 
+{
+    public GameObject spell;
+    public float spellForce;
+
+    private Animator anim;
+    private Stat stat; 
+
+	private void Start()
+	{
+        anim = GetComponentInChildren<Animator>();
+	}
+
+	void Update () 
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject newSpell = Instantiate(spell, transform.position, transform.rotation);
+            newSpell.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(spellForce, 0f));
+
+            anim.SetBool("isAttacking", true);
+        }
+        else if(!Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("isAttacking", false);
+        }
+	}
+
+}
