772b5fd057ca0c4c0ef3b285acbb4f371d151e23
Camille <camille.durif@edu.gobelins.fr>
2019-08-04 13:54:03 +0200

    New Camera

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -33,6 +33,8 @@ public class Player : MonoBehaviour
 
     public Renderer[] renderers;
 
+    public CameraController camera;
+
     private void Awake()
     {
         floorMAsk = LayerMask.GetMask("Floor");
@@ -176,7 +178,9 @@ public class Player : MonoBehaviour
     {
         if (!isImmune)
         {
-            cameraAnim.SetTrigger("isShaking");
+
+            camera.ShakeElapsedTime = camera.ShakeDuration;
+
             health.CurrentVal -= damage;
 
             isImmune = true;
